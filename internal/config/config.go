9b7e3a123b25676d00f9c1dea5f00e8a50515249
Xplant2006 <parthivsatish2@gmail.com>
2026-05-21 04:48:13 +0530

    Add basic structs and save and load config

---
+++ internal/config/config.go
@@ -0,0 +1,98 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"encoding/json"
+)
+
+type AppConfig struct {
+	DisplayName string `json:"display_name"`
+	Port        int    `json:"port"`
+	Status      string `json:"status"`
+}
+
+type Device struct {
+	Name      string
+	IP        string
+	Status    string
+	PublicKey string
+	Known     bool
+}
+
+type Message struct {
+	Type string `json:"type"`
+	From string `json:"from,omitempty"`
+	Text string `json:"text,omitempty"`
+	Room string `json:"room,omitempty"`
+	Ts   int64  `json:"ts,omitempty"`
+}
+
+func ConfigDir() string {
+	homedir,_ := os.UserHomeDir()
+	switch runtime.GOOS {
+	case "windows":
+		appdata := os.Getenv("APPDATA")
+		if appdata == ""{appdata=homedir}
+		return filepath.Join(appdata,"lantern")
+	default:
+		return filepath.Join(homedir, ".config", "lantern")
+	}
+}
+
+func DataDir() string {
+	home, _ := os.UserHomeDir()
+	switch runtime.GOOS {
+	case "windows":
+		local := os.Getenv("LOCALAPPDATA")
+		if local == "" { local = home }
+		return filepath.Join(local, "lantern", "history")
+	default:
+		return filepath.Join(home, ".local", "share", "lantern", "history")
+	}
+}
+
+func Save(cfg AppConfig) error{
+	appconfigbytes,err:=json.MarshalIndent(cfg,"","  ")
+	if err!=nil{
+		return err
+	}
+	path := ConfigDir()
+	err=os.MkdirAll(path,0755)
+	if err!=nil{
+		return err
+	}
+	return os.WriteFile(filepath.Join(path,"config.json"),appconfigbytes,0644)
+}
+
+func Load() (AppConfig,error){
+	var cfg AppConfig
+	path:=filepath.Join(ConfigDir(),"config.json")
+	data,err:=os.ReadFile(path)
+	if os.IsNotExist(err){
+		fmt.Println("Enter your display name: ")
+		var name string
+		_,err=fmt.Scan(&name)
+		cfg=AppConfig{
+			DisplayName: name,
+			Port: 5000,
+			Status: "Active",
+		}
+		err=Save(cfg)
+		if err!=nil{
+			return AppConfig{},err
+		}
+		return cfg,nil
+	}else if err!=nil{
+		return AppConfig{},err
+	}
+	err=json.Unmarshal(data,&cfg)
+	if err!=nil{
+		return AppConfig{},err
+	}
+
+	return cfg,nil
+	
+}
